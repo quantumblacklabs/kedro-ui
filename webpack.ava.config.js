40566d3202427b403b820ef867406eddb84da912
Andy Shora <andy.shora@qb0003-mac-2.local>
2017-01-18 14:50:01 +0000

    Attempt at setting babel-register options.

--- webpack.ava.config.js
+++ webpack.ava.config.js
@@ -1,3 +1,9 @@
+require('babel-register')({
+  // This will override `node_modules` ignoring - you can alternatively pass
+  // an array of strings to be explicitly matched or a regex / glob
+  ignore: false
+});
+
 var config = require('@quantumblack/javascript-standards/config/ava/webpack.ava.config.js');
 
 module.exports = config;
