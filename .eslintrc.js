19ac5e5a121dae50925e003cbf59464bdf4af65c
Richard Westenra <rjwestenra@gmail.com>
2017-02-20 17:28:30 +0000

    Disable  eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 var baseConfig = require('@quantumblack/javascript-standards/config/react/eslint/.eslintrc-source');
 
 baseConfig.rules = Object.assign(baseConfig.rules, {
-  'react/jsx-filename-extension': [ 1, { 'extensions': ['.js', '.jsx'] } ]
+  'react/jsx-filename-extension': [ 1, { 'extensions': ['.js', '.jsx'] } ],
+  'react/jsx-boolean-value': ['off']
 });
 
 baseConfig.settings = Object.assign(baseConfig.settings, {
