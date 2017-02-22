0ff49877a034a32f9efa8cd1befd25ec0e887f8b
Andy Shora <andy.shora@qb0003-mac-2.local>
2017-02-22 11:21:48 +0000

    Output format fixed, can now be consumed via imports in other libs.

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,3 +1,67 @@
-var config = require('@quantumblack/javascript-standards/config/react/webpack/webpack.build.config.js');
+'use strict';
 
-module.exports = config;
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: './dist',
+    filename: 'index.js',
+    publicPath: './dist',
+    libraryTarget: 'commonjs2',
+    library: 'flames'
+  },
+  externals: {
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
+  },
+  resolve: {
+    modules: [
+      __dirname,
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx', '.css']
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: false
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('stylelint'),
+                  require('postcss-cssnext')
+                ];
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        loaders: ['babel-loader', 'svg-react-loader']
+      }
+    ]
+  }
+};
