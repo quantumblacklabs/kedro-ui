c9d92fc3c89dd9ca1e7b27ed5c302f25cf6bc65f
Andy Shora <andy.shora@qb0003-mac-2.local>
2017-03-08 11:48:09 +0000

    Fixed ignore paths

---
+++ templates/custom-renderers/styleguide/index.jsx
@@ -0,0 +1,38 @@
+import React, { PropTypes } from 'react';
+import cx from 'classnames';
+
+import './styles.css';
+
+const StyleGuideRenderer = ({
+  classes,
+  title,
+  homepageUrl,
+  components,
+  toc,
+  sidebar
+}) =>  (
+	<div className={cx('cbn-sg-root', sidebar && 'cbn-sg-root--sidebar-open')}>
+		<main className='cbn-sg-content'>
+			<div className='cbn-sg-components'>
+				{components}
+			</div>
+		</main>
+		{sidebar && false &&
+			<div className='cbn-sg-sidebar'>
+				<h1>{title}</h1>
+				{toc}
+			</div>
+		}
+	</div>
+);
+
+StyleGuideRenderer.propTypes = {
+  classes: PropTypes.object,
+	title: PropTypes.string.isRequired,
+	homepageUrl: PropTypes.string.isRequired,
+	components: PropTypes.node.isRequired,
+	toc: PropTypes.node.isRequired,
+	sidebar: PropTypes.bool
+};
+
+export default StyleGuideRenderer;
