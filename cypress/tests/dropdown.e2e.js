6e1265c9517fa34684147823904cc98c3e60f1e9
Andy Shora <andy.shora@qb0003-mac-2.local>
2017-03-09 09:41:59 +0000

    Added wait to test CircleCI bug

--- cypress/tests/dropdown.e2e.js
+++ cypress/tests/dropdown.e2e.js
@@ -5,7 +5,9 @@ describe('Dropdown component', () => {
   });
 
   it('assert that we have more than 2 demos rendering root nodes correctly', () => {
-    cy.get('.cbn-dropdown')
+    cy
+      .wait(5000)
+      .get('.cbn-dropdown')
       .should('to.have.length.above', 2);
   });
 
