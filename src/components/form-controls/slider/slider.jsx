7b0b93c702293f9aacf2ebe766c39fe9f5fed999
Lucia Kocincova <Lucia.Kocincova@quantumblack.com>
2017-04-07 16:56:16 +0100

    Added basic JSX and CSS

---
+++ src/components/form-controls/slider/slider.jsx
@@ -0,0 +1,86 @@
+import React, { PropTypes } from 'react';
+
+import './slider.css';
+
+/**
+ * Creates a slider component.
+ */
+class Slider extends React.Component {
+  /**
+   * constructor - create new component with given props.
+   * @param  {object} props
+   */
+  constructor(props) {
+    super(props);
+
+    this.displayName = 'Slider';
+
+    this.state = {
+    };
+
+    this._handleChanged = this._handleChanged.bind(this);
+  }
+
+  /**
+   * _handleChanged - updates the state with the value from the slider and triggers the passed on change callback.
+   * @param  {object} event
+   */
+  _handleChanged(event) {
+    if (typeof this.props.onChange === 'function') {
+      this.props.onChange(event);
+    }
+  }
+
+  /**
+   * React lifecycle method
+   * {@link https://facebook.github.io/react/docs/react-component.html#render}
+   * @return {object} JSX for this component
+   */
+  render() {
+    return (
+      <div className='cbn-slider'>
+        <input
+          type='range'
+          name={this.props.name}
+          min={this.props.min}
+          max={this.props.max}
+          step={this.props.step} />
+      </div>
+    );
+  }
+}
+
+Slider.defaultProps = {
+  max: 100,
+  min: 0,
+  name: 'slider',
+  onChange: null,
+  step: 1
+};
+
+Slider.propTypes = {
+  /**
+   * Minimal value of the slider.
+   */
+  min: PropTypes.number,
+  /**
+   * Maximum value of the slider.
+   */
+  max: PropTypes.number,
+  /**
+   * Name of the slider.
+   * NOTE: SHOULD THE NAME BE INCLUDED? OR BE REMOVED?
+   * FROM MDN: "The name of the control, which is submitted with the form data."
+   */
+  name: PropTypes.string,
+  /**
+   * Event listener which will be trigerred on change of the slider.
+   */
+  onChange: PropTypes.func,
+  /**
+   * Step of the slider.
+   */
+  step: PropTypes.number
+};
+
+export default Slider;
