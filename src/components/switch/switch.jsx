4326a6b49bda625db14ca4f4ba0e263b39d9808d
Ottis Kelleghan <ottis.kelleghan@quantumblack.com>
2017-05-05 12:06:47 +0100

    Fixing theming issue with switches

--- src/components/switch/switch.jsx
+++ src/components/switch/switch.jsx
@@ -46,7 +46,7 @@ const Switch = ({ checked,
   const _containerClassNames = classnames(
     'cbn-switch',
     `cbn-switch-${type}`,
-    `cbn-switch-${type}--${theme}`,
+    `cbn-theme--${theme}`,
     extraClasses
   );
 
