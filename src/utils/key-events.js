bea841317d74f36d1b87b3f4be4bc0aff401328f
Richard Westenra <rjwestenra@gmail.com>
2017-05-11 15:54:18 +0100

    Allow key-events util to apply one event to multiple comma-separated values

--- src/utils/key-events.js
+++ src/utils/key-events.js
@@ -13,26 +13,36 @@ const KEYS = {
  * Convenience function for handling keyCodes and creating actions
  * @param  {number} keyCode - A keyboard character
  * @param  {Object} [keyActions] - An optional object-literal list of key names and actions
- * @return {Function|undefined} Either a function for a given key char, or nothing
+ * @return {Function|object} Either a function for a given key char, or nothing
  */
 const handleKeyEvent = (keyCode, keyActions) => {
   /**
-   * A function to execute a callback when a given key name matches the key code received
+   * Execute a callback if a given key name matches the key code received
    * @param {string}   keyName  - A key name string (e.g. 'left')
    * @param {Function} callback - A function to execute if the key name matches the key code
    */
   const handleSingleKey = (keyName, callback) => {
     if (keyCode in KEYS && KEYS[keyCode] === keyName.toLowerCase()) {
-      callback();
+      return callback();
     }
+
+    return undefined;
   };
 
   if (keyActions) {
     return Object.keys(keyActions)
-      .forEach(key => handleSingleKey(key, keyActions[key]));
+      .map(key => handleSingleKey(key, keyActions[key]));
   }
 
-  return handleSingleKey;
+  // Handle a single key, or a comma-separated list of keys
+  return (key, callback) => {
+    if (key.includes(',')) {
+      return key.split(/\s*,\s*/)
+        .map(k => handleSingleKey(k, callback));
+    }
+
+    return handleSingleKey(key, callback);
+  };
 };
 
 export default handleKeyEvent;
