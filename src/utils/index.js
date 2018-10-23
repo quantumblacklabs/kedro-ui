b373a9b6187a921162b931d2bf12edcbdc92fd41
Richard Westenra <rjwestenra@gmail.com>
2018-10-23 17:01:25 +0100

    Add search-results-utils to main utils export

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,15 @@
 import CustomPropTypes from './prop-types';
 import handleKeyEvent from './key-events';
+import {
+  escapeRegExp,
+  getValueRegex,
+  getHighlightedText
+ } from '../components/search-results/search-results-utils';
 
 module.exports = {
   CustomPropTypes,
-  handleKeyEvent
+  handleKeyEvent,
+  escapeRegExp,
+  getValueRegex,
+  getHighlightedText
 };
