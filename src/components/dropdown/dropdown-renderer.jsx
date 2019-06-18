c8c980ea49906e95e5599f925eb48be1c7f688b5
Richard Westenra <rjwestenra@gmail.com>
2019-06-18 17:44:30 +0100

    Optimise lodash imports

--- src/components/dropdown/dropdown-renderer.jsx
+++ src/components/dropdown/dropdown-renderer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import utils from '../../utils';
-import { uniqueId } from 'lodash';
+import uniqueId from 'lodash/uniqueId';
 
 // Components
 import Icon from '../icon';
