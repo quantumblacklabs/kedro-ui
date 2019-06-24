ba757031413851e0dd51378b7a1ca538d52f78b7
Richard Westenra <rjwestenra@gmail.com>
2019-06-24 15:36:59 +0100

    Fix icon SVGs

--- src/components/icon/assets/index.js
+++ src/components/icon/assets/index.js
@@ -1,12 +1,12 @@
-import { ReactComponent as darkCopy } from './dark-copy.svg';
-import { ReactComponent as darkCut } from './dark-cut.svg';
-import { ReactComponent as darkPaste } from './dark-paste.svg';
-import { ReactComponent as darkRefresh } from './dark-refresh.svg';
-import { ReactComponent as darkUndo } from './dark-undo.svg';
-import { ReactComponent as darkSortingAscending } from './dark-sorting-ascending.svg';
-import { ReactComponent as tick } from './tick.svg';
-import { ReactComponent as search } from './search.svg';
-import { ReactComponent as close } from './close.svg';
+import darkCopy from './dark-copy.svg';
+import darkCut from './dark-cut.svg';
+import darkPaste from './dark-paste.svg';
+import darkRefresh from './dark-refresh.svg';
+import darkUndo from './dark-undo.svg';
+import darkSortingAscending from './dark-sorting-ascending.svg';
+import tick from './tick.svg';
+import search from './search.svg';
+import close from './close.svg';
 
 export default {
   close,
