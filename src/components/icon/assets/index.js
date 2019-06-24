671ebda51654ec0f31d3c237df7e8694ecf74d87
Richard Westenra <rjwestenra@gmail.com>
2019-06-24 15:41:04 +0100

    Rename icons to remove 'dark' from filename

--- src/components/icon/assets/index.js
+++ src/components/icon/assets/index.js
@@ -1,21 +1,21 @@
-import darkCopy from './dark-copy.svg';
-import darkCut from './dark-cut.svg';
-import darkPaste from './dark-paste.svg';
-import darkRefresh from './dark-refresh.svg';
-import darkUndo from './dark-undo.svg';
-import darkSortingAscending from './dark-sorting-ascending.svg';
+import copy from './copy.svg';
+import cut from './cut.svg';
+import paste from './paste.svg';
+import refresh from './refresh.svg';
+import undo from './undo.svg';
+import chevronUp from './sorting-ascending.svg';
 import tick from './tick.svg';
 import search from './search.svg';
 import close from './close.svg';
 
 export default {
   close,
-  copy: darkCopy,
-  cut: darkCut,
-  paste: darkPaste,
-  refresh: darkRefresh,
-  undo: darkUndo,
-  chevronUp: darkSortingAscending,
+  copy,
+  cut,
+  paste,
+  refresh,
+  undo,
+  chevronUp,
   search,
   tick
 };
